d4f4f3505c848aad826a553b166fcef36cba667f
Alexis Peña <alexxis.aaps11@gmail.com>
2025-01-12 09:31:51 -0300

    Release 1.2.1: - Updated version numbers in config files - Made goal/step text selectable - Streamlined settings dialog - Added bug reporting section - Fixed dialog sizing issues

--- src/config.py
+++ src/config.py
@@ -3,5 +3,5 @@ Configuration constants for the Goal Tracker application
 """
 
 APP_NAME = "Goal Tracker"
-APP_VERSION = "1.2.0"
+APP_VERSION = "1.2.1"
 APP_ID = "io.github.alexxisaapps.elementary_goal_tracker"
